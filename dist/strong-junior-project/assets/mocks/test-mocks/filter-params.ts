70dbd419bd9c5ee205017408575a9de7973777d3
andrii-paslavskyi <andriipas200017@gmail.com>
2023-04-02 12:57:04 +0300

    release 1.0.0

---
+++ dist/strong-junior-project/assets/mocks/test-mocks/filter-params.ts
@@ -0,0 +1,18 @@
+export const filterParamsEmpty = {
+  search: '',
+  platforms: '',
+  ordering: '',
+  metacritic: '',
+  developers: '',
+  dates: '',
+  genres: ''
+}
+export const filterParamsWithData = {
+  search: 'Hero',
+  platforms: '',
+  ordering: '',
+  metacritic: '',
+  developers: '',
+  dates: '',
+  genres: ''
+}
