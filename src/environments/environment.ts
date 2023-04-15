3c4854a213717e01d24036d5473358863e887e87
Andrii_Paslavskyi <andrii.paslavskyi@sombrainc.com>
2023-04-16 00:13:05 +0300

    add onPush strategy

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'games-app-angular-project',
+    appId: '1:47406338973:web:9c728362f3209292549574',
+    databaseURL: 'https://games-app-angular-project-default-rtdb.firebaseio.com',
+    storageBucket: 'games-app-angular-project.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAkbRGnqbjCQMEU375NwVzy8rC36nXQi6g',
+    authDomain: 'games-app-angular-project.firebaseapp.com',
+    messagingSenderId: '47406338973',
+  },
+  production: false,
+};
