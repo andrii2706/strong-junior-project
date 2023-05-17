895c270529c70b3c12456c3e48fc45d4ccc7e23d
Andrii_Paslavskyi <andrii.paslavskyi@sombrainc.com>
2023-05-17 22:13:36 +0300

    user update

---
+++ src/app/pages/admin/containers/update-user/update-user.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-user',
+  templateUrl: './update-user.component.html',
+  styleUrls: ['./update-user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UpdateUserComponent {
+
+}
